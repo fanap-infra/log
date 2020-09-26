82de49ff9257f75a9c1a9827eddd44bfe7a9bd9f
Hossein Daneshvar <daneshvar.ho@gmail.com>
2020-09-26 14:38:55 +0330

    better performance

--- writer.go
+++ writer.go
@@ -1,15 +1,9 @@
 package log
 
-import "sync"
-
 type encoder interface {
 	Print(l Level, s string, caller string, stacks []string, message string)
 	Printv(l Level, s string, caller string, stacks []string, message string, keysValues []interface{})
 	close()
-}
-type enablerFuncParam struct {
-	l Level
-	s string
 }
 
 type Writer struct {
@@ -17,9 +11,6 @@ type Writer struct {
 	enabler EnablerFunc
 	stack   EnablerFunc
 	caller  bool
-
-	enablerCache sync.Map
-	stackCache   sync.Map
 }
 
 func newWriter(enabler EnablerFunc, stack EnablerFunc, caller bool, encoder encoder) *Writer {
@@ -28,34 +19,13 @@ func newWriter(enabler EnablerFunc, stack EnablerFunc, caller bool, encoder enco
 		enabler: enabler,
 		caller:  caller,
 		stack:   stack,
-
-		// enablerCache: make(map[enablerFuncParam]bool),
-		// stackCache:   make(map[enablerFuncParam]bool),
 	}
 }
 
 func (w *Writer) isEnable(level Level, scope string) bool {
-	p := enablerFuncParam{level, scope}
-
-	if r, ok := w.enablerCache.Load(p); ok {
-		return r.(bool)
-	}
-
-	r := w.enabler(level, scope)
-	w.enablerCache.Store(p, r)
-
-	return r
+	return w.enabler(level, scope)
 }
 
 func (w *Writer) isStack(level Level, scope string) bool {
-	p := enablerFuncParam{level, scope}
-
-	if r, ok := w.stackCache.Load(p); ok {
-		return r.(bool)
-	}
-
-	r := w.stack(level, scope)
-	w.stackCache.Store(p, r)
-
-	return r
+	return w.stack(level, scope)
 }
